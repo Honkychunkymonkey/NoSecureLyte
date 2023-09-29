62700edbd24d86d525a89d51394e0dd8d9b2832d
Honkychunkymonkey <87286565+Honkychunkymonkey@users.noreply.github.com>
2023-09-29 20:50:22 +0000

    Moving to bun, no more node :)

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const express = require("express");
 const http = require("http");
 const Unblocker = require("unblocker");
 const compression = require("compression");
-const cluster = require("cluster");
-const numCPUs = require("os").cpus().length;
 const ws = require("ws");
+
 // Importing our custom modules for storage, configuration and WebSocket handling
 const storageRoutes = require("./storageRoutes");
 const config = require("./config");
@@ -22,112 +21,99 @@ const pinoHttp = require("pino-http");
 const app = express();
 const server = http.createServer(app);
 
-if (cluster.isMaster) {
-  console.log(`Master ${process.pid} is running`);
-
-  // Fork workers.
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
-
-  cluster.on("exit", (worker, code, signal) => {
-    console.log(`worker ${worker.process.pid} died`);
-  });
-} else {
-  // Endpoint to get the prefix value from the config
-  app.get("/prefix", (req, res) => {
+// Endpoint to get the prefix value from the config
+app.get("/prefix", (req, res) => {
     res.send({ prefix: config.PREFIX });
-  });
+});
 
-  // Function to check config and log errors
-  function checkConfig() {
+// Function to check config and log errors
+function checkConfig() {
     if (!config.PREFIX) {
-      console.error("Configuration error: PREFIX is not set.");
+        console.error("Configuration error: PREFIX is not set.");
     }
 
     if (!config.PORT || config.PORT === 0) {
-      // Generate a random port number between 3000 and 9999 if port is 0 or not set
-      config.PORT = Math.floor(Math.random() * (9999 - 3000 + 1) + 3000);
-      console.log(
-        `Port is not set or 0, random port ${config.PORT} has been generated.`
-      );
+        // Generate a random port number between 3000 and 9999 if port is 0 or not set
+        config.PORT = Math.floor(Math.random() * (9999 - 3000 + 1) + 3000);
+        console.log(
+            `Port is not set or 0, random port ${config.PORT} has been generated.`
+        );
     } else if (config.PORT.toString().length < 3) {
-      console.error("Configuration error: PORT should be at least 3 digits.");
+        console.error("Configuration error: PORT should be at least 3 digits.");
     } else if (config.PORT.toString().length > 4) {
-      console.error("Configuration error: PORT should be at most 4 digits.");
+        console.error("Configuration error: PORT should be at most 4 digits.");
     }
-  }
+}
 
-  // Run the config check function
-  checkConfig();
+// Run the config check function
+checkConfig();
 
-  // Initialize logger
-  const logger = pino({ level: "warn" });
-  const httpLogger = pinoHttp({ logger });
+// Initialize logger
+const logger = pino({ level: "warn" });
+const httpLogger = pinoHttp({ logger });
 
-  // Use imported storage routes on "/storage" path
-  app.use("/storage", storageRoutes);
-  // Use middleware for logging, compression, security, and serving static files
-  app.use(compression());
-  app.use(httpLogger);
-  app.use(
+// Use imported storage routes on "/storage" path
+app.use("/storage", storageRoutes);
+// Use middleware for logging, compression, security, and serving static files
+app.use(compression());
+app.use(httpLogger);
+app.use(
     helmet({
-      contentSecurityPolicy: false,
+        contentSecurityPolicy: false,
     })
-  );
-  app.use(express.static("public"));
+);
+app.use(express.static("public"));
 
-  // Initialize Unblocker middleware for proxy functionality, including URL prefix and response middleware
-  const unblocker = new Unblocker({
+// Initialize Unblocker middleware for proxy functionality, including URL prefix and response middleware
+const unblocker = new Unblocker({
     prefix: config.PREFIX,
     responseMiddleware: [
-      async function (data) {
-        // This function handles HTML response modifications before it reaches the client
-      },
+        async function (data) {
+            // This function handles HTML response modifications before it reaches the client
+        },
     ],
-  });
+});
 
-  // Set port from configuration
-  app.set("port", config.PORT);
+// Set port from configuration
+app.set("port", config.PORT);
 
-  // Handle WebSocket upgrade requests
-  server.on("upgrade", (request, socket, head) => {
+// Handle WebSocket upgrade requests
+server.on("upgrade", (request, socket, head) => {
     wss.handleUpgrade(request, socket, head, (ws) => {
-      wss.emit("connection", ws, request);
+        wss.emit("connection", ws, request);
     });
-  });
+});
 
-  // Middleware to sanitize HTML in responses
-  const sanitizeHtmlMiddleware = (req, res, next) => {
+// Middleware to sanitize HTML in responses
+const sanitizeHtmlMiddleware = (req, res, next) => {
     const sendResponse = res.send;
     res.send = (body) => {
-      const sanitizedBody = sanitizeHtml(body);
-      sendResponse.call(res, sanitizedBody);
+        const sanitizedBody = sanitizeHtml(body);
+        sendResponse.call(res, sanitizedBody);
     };
     next();
-  };
+};
 
-  // Use Unblocker and sanitize HTML middleware
-  app.use(unblocker);
-  app.use(sanitizeHtmlMiddleware);
+// Use Unblocker and sanitize HTML middleware
+app.use(unblocker);
+app.use(sanitizeHtmlMiddleware);
 
-  // Handle "/go/:url" path for proxying or WebSockets
-  app.get("/go/:url", (req, res) => {
+// Handle "/go/:url" path for proxying or WebSockets
+app.get("/go/:url", (req, res) => {
     const url = decodeURIComponent(req.params.url);
-  });
+});
 
-  // Redirect to "/go/:url" path when "/go" path is accessed with a URL parameter
-  app.get("/go", (req, res) => {
+// Redirect to "/go/:url" path when "/go" path is accessed with a URL parameter
+app.get("/go", (req, res) => {
     const url = req.query.url;
     if (!url) {
-      return res.status(400).send("Missing URL parameter");
+        return res.status(400).send("Missing URL parameter");
     }
     const proxyUrl = `/go/${encodeURIComponent(url)}`;
     res.redirect(proxyUrl);
-  });
+});
 
-  // Start server listening on the specified port
-  server.listen(app.get("port"), () => {
-    console.log(`Server running on port ${app.get("port")}`);
-  });
-}
+// Start server listening on the specified port
+server.listen(app.get("port"), () => {
+    console.log(`Web server is now running on port ${app.get("port")}`);
+});
